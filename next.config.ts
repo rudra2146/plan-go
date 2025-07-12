f7fa44288e668e3af80d4fe954140967b5cc5b4d
Rudra Pandya <viralpandya079@gmail.com>
2025-07-12 12:50:30 +0530

    Solving issue

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,33 @@
-import type { NextConfig } from "next";
+// next.config.ts
 import path from "path";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "**.ufs.sh",
-      },
-    ],
+        hostname: "**.ufs.sh"
+      }
+    ]
   },
-  webpack(config, { isServer }) {
-    // Skip system paths that cause permission issues
-    config.module.rules.push({
-      test: /\.(js|ts|tsx)$/,
-      exclude: [
-        /node_modules/,
+  webpack(config) {
+    config.snapshot = {
+      ...config.snapshot,
+      managedPaths: [
         path.resolve("C:/Users/Home/Application Data"),
         path.resolve("C:/Users/Home/Cookies"),
       ],
-    });
-
+    };
+    config.watchOptions = {
+      ignored: [
+        "**/node_modules",
+        "**/.next",
+        path.resolve("C:/Users/Home/Application Data"),
+        path.resolve("C:/Users/Home/Cookies"),
+      ]
+    };
     return config;
-  },
+  }
 };
 
 export default nextConfig;
