c844342c21ad83843af9eebfe8ebc146e23920d8
Rudra Pandya <viralpandya079@gmail.com>
2025-07-16 19:56:13 +0530

    Auth bug solving

--- auth.ts
+++ auth.ts
@@ -1,8 +1,15 @@
 import NextAuth from 'next-auth'
-import GitHub from "next-auth/providers/github"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import {prisma} from '@/lib/prisma'
-export const {auth, handlers, signIn, signOut} = NextAuth({
-    providers: [GitHub],
-    adapter: PrismaAdapter(prisma),
+import GitHubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/lib/prisma'
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
 })
